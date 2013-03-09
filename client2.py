77917b86cc1c8ccefa3d4dee36fb3282515c6391
Rafael Arrais <rafael.arrais@gmail.com>
2013-03-09 01:24:50 +0000

    Server receives connections

--- client2.py
+++ client2.py
@@ -1,11 +1,36 @@
 # Echo client program
 import socket
+import sys
 
-HOST = 'localhost'    # The remote host
+HOST = "localhost"    # The remote host
 PORT = 50007              # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-s.sendall('Hello, world')
-data = s.recv(1024)
-s.close()
+ADDR = (HOST,PORT)
+BUFSIZE = 4096
+
+try:
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error as msg:
+	client = None
+	print "Failed to create socket"
+	print "Error Code: " + str(msg[0])
+	print "Error Message: " + str(msg[1])
+	sys.exit();
+print "Socket Created!"
+
+
+try:
+	client.connect((HOST, PORT))
+except socket.error as msg:
+	print "Failed to connect!"
+	print "Error code: " + str(msg[0])
+	print "Error Message: " + str(msg[1])
+	sys.exit();
+print "Socket Connected to " + HOST + " on PORT " + str(PORT)
+
+
+
+client.send('Hello, world')
+
+data = client.recv(1024)
+
 print 'Received', repr(data)
--- server2.py
+++ server2.py
@@ -11,7 +11,7 @@ try:
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 except socket.error as msg:
     server = None
-    print "Failed to create socket"
+    print "Failed to create socket!"
     print "Error Code: " + str(msg[0])
     print "Error Message: " + str(msg[1])
     sys.exit();
@@ -21,23 +21,37 @@ print "Socket Created!"
 try:
     server.bind((ADDR))
 except socket.error as msg:
-    print "Failed to bind socket."
+    print "Failed to bind socket!"
     print "Error Code: " + str(msg[0])
     print "Error Message: " + str(msg[1])
     sys.exit();
 print "Socket Bind complete!"
 
-
-
 server.listen(5)
-print "listening..."
+print "listening on PORT " + str(PORT) + "..."
 
 
-conn, addr = server.accept()
-print 'Connected by', addr
-conn.send("TEST!")
 while 1:
+    conn, addr = server.accept()
+    print "Connected with " + addr[0] + ":" + str(addr[1])
+
     data = conn.recv(1024)
-    if not data: break
-    conn.sendall(data)
+    # reply = "Ok..." + data
+    # if not data:
+    #     break
+
+    # conn.sendall(reply)
+    print data
+
 conn.close()
+server.close()
+
+# conn.send("TEST!")
+
+
+
+# while 1:
+#     data = conn.recv(1024)
+#     if not data: break
+#     conn.sendall(data)
+# conn.close()
